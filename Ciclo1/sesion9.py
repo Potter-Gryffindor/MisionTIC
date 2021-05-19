9eaf5015f438d0005e3042a2c262bccedb89725e
Javier Bayter Consuegra <70031070+Potter-Gryffindor@users.noreply.github.com>
2021-05-19 13:41:39 -0500

    Componente práctica, sesión 9

--- Ciclo1/sesion9.py
+++ Ciclo1/sesion9.py
@@ -39,11 +39,20 @@ def ejemplo2():
 
 def actividad1():
     print("actividad1")
-    # Vamos a realizar un algoritmo que nos calcule la serie de Fibonacci.
+        # Vamos a realizar un algoritmo que nos calcule la serie de Fibonacci.
     # La serie o secuencia de Fibonacci comienza con los números 0 y 1 y 1, y a partir de allí es posible calcular el siguiente valor como la suma de los dos valores anteriores. 
     # Por ejemplo, 1+1=2, 1+2=3, 2+3=5, etc. Así, los primeros números de la secuencia son: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
     # Creemos un programa que a partir de un número N ingresado, imprima los primeros N números de la serie de Fibonacci.
- 
+    N = int(input('N: '))
+    a = 0
+    b = 1
+    print(f'Primeros {N} números de la serie de Fibonacci: ')
+    for i in range(N):
+        print(a,end= ', ' if i<N-1 else '' )
+        aux = a+b
+        a = b
+        b = aux
+        
 #actividad1()
 
 def actividad2():
