195b6f2dd556b43d2ba8c7d00b1a1cfa9a3911ff
Javier Bayter Consuegra <70031070+Potter-Gryffindor@users.noreply.github.com>
2021-07-15 13:35:51 -0500

    Add files via upload

--- Ciclo2/sesion1/src/Ejemplo7.java
+++ Ciclo2/sesion1/src/Ejemplo7.java
@@ -3,25 +3,30 @@ public class Ejemplo7 {
     public static void main(String[] args) throws Exception {
         int tipoProduct = 0,cantProduct = 0, clientes = 0;
         double total = 0, valor = 0, totalVendido = 0;
+        char opcion;
         Scanner Leer = new Scanner(System.in);
-        System.out.print("Clientes: ");
-        clientes = Leer.nextInt();
-        for(int i=1;i<=clientes;i++){
-            System.out.println("Cliente "+i+": ");
+        do{
+            clientes++;
+            System.out.println("Cliente "+clientes+": ");
             System.out.print("Cantidad de tipo de productos: ");
-            tipoProduct = Leer.nextInt();
+            tipoProduct = Integer.parseInt(Leer.nextLine());
             total = 0;
             for(int j=1;j<=tipoProduct;j++){
-                System.out.print("Cantidad del producto "+j+":");
-                cantProduct = Leer.nextInt();
-                System.out.print("Valor del producto "+j+":");
-                valor = Leer.nextInt();
+                System.out.print("Cantidad del producto "+j+": ");
+                cantProduct = Integer.parseInt(Leer.nextLine());
+                System.out.print("Valor del producto "+j+": ");
+                valor = Integer.parseInt(Leer.nextLine());
                 total += cantProduct*valor;
             }
+            if (total>10e4)
+                total *= 0.90; 
             totalVendido += total; 
             System.out.println("Total de la factura: "+total);
-        }       
+            System.out.print("Desea continuar(S/N): ");
+            opcion =Leer.nextLine().charAt(0);
+        }while(opcion=='S');
         System.out.println("Total vendido: "+totalVendido);
-        Leer.close();
+        System.out.println("Clientes atendidos: "+clientes);
+        Leer.close();    
     }
 }
